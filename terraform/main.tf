038c34c1927f9b216f635b6549ffd689bab45182
fuekeng <fuekengbilly@gmail.com>
2026-02-17 15:48:57 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "myapp" {
   name       = "hello-svc"
-  repository = "https://devops4solutions.github.io/springboot-helm-chart/" # Using the published Helm repo
+  repository = "https://fuekeng.github.io/springboot-helm-chart/" # Using the published Helm repo
   chart      = "springboot"
   namespace  = "app1"  # Change as per your setup
   version    = "0.1.5"    # Use the correct version from index.yaml!
