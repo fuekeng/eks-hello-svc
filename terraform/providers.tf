8945e58cdae15254dae91f7564deaa541ae3016f
fuekeng <fuekengbilly@gmail.com>
2026-03-06 01:17:47 +0100

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,8 +7,8 @@ provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.cluster.endpoint
     token                  = data.aws_eks_cluster_auth.cluster.token
-    insecure = true
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    insecure = false
+    # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   }
 }
 terraform {
